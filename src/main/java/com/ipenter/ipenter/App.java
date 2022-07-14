87bd5a7febd7d908738960c0395c037290184a5c
hafizip00 <hafizip00@gmail.com>
2022-07-15 01:33:13 +0500

    Maven Spring

---
+++ src/main/java/com/ipenter/ipenter/App.java
@@ -0,0 +1,14 @@
+package com.ipenter.ipenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Car car = new Car();
+        car.drive();
+    }
+}
---
+++ src/main/java/com/ipenter/ipenter/Car.java
@@ -0,0 +1,7 @@
+package com.ipenter.ipenter;
+
+public class Car {
+	public void drive() {
+		System.out.println("Driving a Car");
+	}
+}
